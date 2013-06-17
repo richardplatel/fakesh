50772b60e5329a73735f44e07c2ced1be1e335f8
Richard Platel <richard.platel+github@gmail.com>
2013-06-16 21:17:16 -0400

    Shell prints a prompt

--- fakesh.rb
+++ fakesh.rb
@@ -2,4 +2,12 @@ class Fakesh
     def do_command(cmd)
         ". .."
     end
+
+    def run(output)
+        output.write "(>^.^)> "
+    end
+end
+
+if __FILE__ == $PROGRAM_NAME
+    Fakesh.new().run(STDOUT)
 end
--- fakesh_spec.rb
+++ fakesh_spec.rb
@@ -6,5 +6,13 @@ describe Fakesh do
             expect(Fakesh.new.do_command('ls')).to eq(". ..")
         end
     end
+    describe '#run' do
+        it 'prints a prompt to output' do
+            output = StringIO.new
+            op = double("output")
+            op.should_receive(:write).with('(>^.^)> ')
+            Fakesh.new.run(op)
+        end
+    end
 end
 
